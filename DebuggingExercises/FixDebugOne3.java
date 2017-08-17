68dfb1b6ac995c888e3b1ffd739109a8d67dde24
jc428225 <bonnie.thompson@my.jcu.edu.au>
2017-08-18 09:57:59 +1000

    Fixed Debugging

---
+++ DebuggingExercises/FixDebugOne3.java
@@ -0,0 +1,9 @@
+public class FixDebugOne33
+{
+   public static void main(String[] args)
+   {
+      System.out.print("Over the river");
+      System.out.print("and through the woods");
+      System.out.print("to Grandmother's house we go");
+   }
+}
